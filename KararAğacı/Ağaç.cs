c2dffadda04fb694fd83d9feecf64caeed0f90cb
erenosman <erenaygun244042@gmail.com>
2016-12-07 21:34:28 +0300

    ağaç yeni değer

--- KararAğacı/Ağaç.cs
+++ KararAğacı/Ağaç.cs
@@ -8,23 +8,9 @@ namespace KararAğacı
 {
     class Ağaç
     {
-        string Nitelik;
-        List<Ağaç> Dallar;
-        string değeri;
-        public void düğümekle(Ağaç düğüm)
-        {
-            Dallar.Add(düğüm);
-        }
-        public Ağaç hangisi(string soru)
-        {
-            foreach (var item in Dallar)
-            {
-                if (item.değeri == soru)
-                {
-                    return item;
-                }
-            }
-            return null;
-        }
+        public string değer = "";
+        public string isim = "";
+        public string sınıf = "";
+        public List<Ağaç> AltListe = new List<Ağaç>();
     }
 }
