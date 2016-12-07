c4739f355d26e65bf7b2a2447a08ffcd6b8881b4
Eren Aygün <erenaygun244042@gmail.com>
2016-12-07 21:17:06 +0300

    ilk

---
+++ KararAğacı/Ağaç.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KararAğacı
+{
+    class Ağaç
+    {
+        string Nitelik;
+        List<Ağaç> Dallar;
+        string değeri;
+        public void düğümekle(Ağaç düğüm)
+        {
+            Dallar.Add(düğüm);
+        }
+        public Ağaç hangisi(string soru)
+        {
+            foreach (var item in Dallar)
+            {
+                if (item.değeri == soru)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+    }
+}
